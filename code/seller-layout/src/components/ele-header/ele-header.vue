9d99dc4747d9836b79036a25f8fd4b8c8e2496cb
zdylg <you@example.com>
2019-09-16 17:37:47 +0800

    9.16

---
+++ code/seller-layout/src/components/ele-header/ele-header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        ele-header
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ele-header"
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
---
+++ code/seller-layout/src/router/index.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import vueRouter from "vue-router";
+import routes from "@/routes"
+Vue.use(vueRouter);
+const router = new vueRouter({
+    routes,
+    linkActiveClass:"active"
+})
+
+export default router
